b76909f8af1a7f1defec6af91953fa39d21af4da
olamiidee <osibeluwoolamide@gmail.com>
2022-09-10 17:30:46 +0100

    tor

---
+++ Readme.md
@@ -0,0 +1,5 @@
+## Configuration
+
+1. npm install
+2. npm start
+3. you can preview the html with live server or file location.
---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+  content: ["./src/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        moderateViolet: "hsl(263, 55%, 52%)",
+        veryDarkGrayishBlue: "hsl(217, 19%, 35%)",
+        veryDarkBlackishBlue: "hsl(219, 29%, 14%)",
+        lightGray: "hsl(0, 0%, 81%)",
+        lightGrayishBlue: "hsl(210, 46%, 95%)",
+      },
+      fontFamily: {
+        sans: ["Barlow Semi Condensed", "sans-serif"],
+      },
+    },
+  },
+
+  plugins: [require("@tailwindcss/line-clamp")],
+};
